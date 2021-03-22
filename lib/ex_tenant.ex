2ea071f8f97f22dd94b3d7a9a6dc17740e2bb1da
Steve <steve@netflakes.co>
2021-03-22 16:44:26 +1100

    initial repo commit

---
+++ lib/ex_tenant.ex
@@ -0,0 +1,18 @@
+defmodule ExTenant do
+  @moduledoc """
+  Documentation for `ExTenant`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTenant.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_tenant_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTenantTest do
+  use ExUnit.Case
+  doctest ExTenant
+
+  test "greets the world" do
+    assert ExTenant.hello() == :world
+  end
+end
