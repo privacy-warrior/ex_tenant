e39695aa864455877fadecc9b683f10334c6b7c2
Steve <steve@netflakes.co>
2021-05-13 12:34:28 +1000

    updated the migration task

--- lib/mix/tasks/ex_tenant.migrate.ex
+++ lib/mix/tasks/ex_tenant.migrate.ex
@@ -1,12 +1,95 @@
 defmodule Mix.Tasks.ExTenant.Migrate do
+  @moduledoc """
+    Run the tenanted migrations
+  """
   use Mix.Task
+  import Mix.Ecto
+  import ExTenant.PathHelper
 
-  @shortdoc "Runs the repository tenanted migrations"
+  @aliases [
+    n: :step,
+    r: :repo
+  ]
 
-  alias ExTenant.Actions
+  @switches [
+    all: :boolean,
+    step: :integer,
+    to: :integer,
+    quiet: :boolean,
+    prefix: :string,
+    pool_size: :integer,
+    log_sql: :boolean,
+    strict_version_order: :boolean,
+    repo: [:keep, :string],
+    no_compile: :boolean,
+    no_deps_check: :boolean,
+    migrations_path: :keep
+  ]
+
+  @doc """
+    Apply tenant migrations to a tenant with given strategy, in given direction.
+
+  A direction can be given, as the third parameter, which defaults to `:up`
+  A strategy can be given as an option, and defaults to `:all`
+
+  ## Paramaters
+
+    - *direction*: defaults to `:up`
+
+  ## Options
+
+    * `:all` - runs all available if `true`
+    * `:step` - runs the specific number of migrations
+    * `:to` - runs all until the supplied version is reached
+    * `:log` - the level to use for logging. Defaults to `:info`.
+      Can be any of `Logger.level/0` values or `false`.
+
+  """
 
   @impl true
-  def run(args, migrator \\ &Actions.migrate_tenanted/2) do
-    migrator.(args)
+  def run(args, migrator \\ &Ecto.Migrator.run/4) do
+    repos = parse_repo(args)
+
+    {opts, _} = OptionParser.parse!(args, strict: @switches, aliases: @aliases)
+
+    opts =
+      if opts[:to] || opts[:step] || opts[:all],
+        do: opts,
+        else: Keyword.put(opts, :all, true)
+
+    opts =
+      if opts[:quiet],
+        do: Keyword.merge(opts, log: false, log_sql: false),
+        else: opts
+
+    # Start ecto_sql explicitly before as we don't need
+    # to restart those apps if migrated.
+    {:ok, _} = Application.ensure_all_started(:ecto_sql)
+
+    for repo <- repos do
+      ensure_repo(repo, args)
+      # paths = ensure_migrations_paths(repo, opts)
+
+      paths = [tenanted_migrations_path(repo)]
+
+      pool = repo.config[:pool]
+
+      fun =
+        if Code.ensure_loaded?(pool) and function_exported?(pool, :unboxed_run, 2) do
+          &pool.unboxed_run(&1, fn -> migrator.(&1, paths, :up, opts) end)
+        else
+          &migrator.(&1, paths, :up, opts)
+        end
+
+      case Ecto.Migrator.with_repo(repo, fun, [mode: :temporary] ++ opts) do
+        {:ok, _migrated, _apps} ->
+          :ok
+
+        {:error, error} ->
+          Mix.raise("Could not start repo #{inspect(repo)}, error: #{inspect(error)}")
+      end
+    end
+
+    :ok
   end
 end
